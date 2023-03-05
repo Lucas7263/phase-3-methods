e3e617e8a332a597e372c532f54295a1d450015b
Lucas <lucaswoodyard@yahoo.com>
2023-03-05 15:04:20 -0600

    commit tree 2e5d764939f7b402a51a142227dd080969df61e9

--- methods.rb
+++ methods.rb
@@ -1,1 +1,37 @@
 # Your code here!
+
+def greet_programmer 
+    
+     puts "Hello, programmer!"
+        
+end
+
+def greet name 
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+greet("Jimmy")
+
+def greet_with_default name = "programmer!"
+    puts "Hello, #{name}!"
+end
+
+greet_with_default("Naureen")
+
+def add num1, num2 
+    return num1 + num2
+end
+
+add(2, 5)
+
+def halve num1 
+    if num1.class != Integer
+        return nil
+    end
+
+    return num1 / 2 
+end
+
+halve(6)
+halve("six")
